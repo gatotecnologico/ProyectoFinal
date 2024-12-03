9c853b40b849fe0b4cb3224ea8210a70ee8fd1d8
gatoguapo <victor._espinoza@hotmail.com>
2024-12-02 18:40:07 -0700

    ruta para obtener registro por id

--- api_node_typescript_11/src/routes/registroRutas.ts
+++ api_node_typescript_11/src/routes/registroRutas.ts
@@ -7,3 +7,10 @@ router.get('/', async (_req:Request, res:Response) => {
     let registros = await registroServices.obtieneRegistros()
     res.send(registros)
 })
+
+//http://localhost:3001/api/registro/1
+router.get('/:id', async (req:Request, res:Response) => {
+    let registro = await registroServices.encuentraRegistro(Number(req.params.id))
+    res.send(registro) 
+ })
+ 
